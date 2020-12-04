1abff53c421009ad37e7cd423060fb8496b0fb84
Priya12340987 <75341592+Priya12340987@users.noreply.github.com>
2020-12-05 01:16:37 +0530

    more responsive

--- Assets/JS/Explore.js
+++ Assets/JS/Explore.js
@@ -8,16 +8,19 @@ var imagePath ;
 //     $(".new__card__").toggle();
 // })
 
-$("#btn_add_card").on("click",function(){
-    $(".new__card__").append('<input type="text" id="path" class= "p-2 mx-2" placeholder="Enter Image Path">')
-    $(".new__card__").append('<input type="text" id="title" class= "p-2 mx-2"  placeholder="Enter Card Title">')
-    $(".new__card__").append('<input type="text"  id="id_card_body" class= "p-2 mx-2"  placeholder="Enter Card Body">')
-    $(".new__card__").append('<button class="btn btn-outline-danger btn-sm new_card_btn p-3"><i class="far fa-heart"></i></button>')
+$(".buttons").on("click","#btn_add_card",function(){
+// $("#btn_add_card").on("click",function(){
+    $(".new__card__").append('<div class="form-group"><input type="text" id="path" class= " form-control" placeholder="Enter Image Path"></div>');
+    $(".new__card__").append('<div class="form-group"><input type="text" id="title" class= "form-control"  placeholder="Enter Card Title"></div>')
+    $(".new__card__").append('<div class="form-group"><input type="text"  id="id_card_body" class= "form-control"  placeholder="Enter Card Body"></div>')
+    $(".new__card__").append('<div class="form-group"><button class="btn btn-outline-danger btn-sm new_card_btn p-3 "><i class="far fa-heart"></i></button></div>')
+    //$(".new__card__").append('<div class="form-group"><button class="btn btn-outline-primary btn-sm new_card_btn_del p-3 "><i class="far fa-user"></i></button></div>')
     $("#path").keypress(function(event){
     if(event.keyCode === 13){
         imagePath = $("#path").val();
         $("#path").val("");
-        $("#path").remove();      
+        $("#path").remove();  
+        
     
     // $("input[type='text']").fadeOut;
     
@@ -25,6 +28,14 @@ $("#btn_add_card").on("click",function(){
 })
     
     $("#title").keypress(function(event){
+        // $(function() {
+        //     var e = $.Event('keypress');
+        //     e.which = 13; // Character 'A'
+        //     $("#path").trigger(e);
+        // });
+        // $("#path").keypress(function(event)
+        //     {event.keyCode === 13})
+        // $("#path").remove(); 
         if(event.keyCode === 13){
              title = $("#title").val();
             $("#title").val("");
@@ -42,12 +53,38 @@ $("#btn_add_card").on("click",function(){
             
 
     }) 
+    
+
 
    
-    $(".new_card_btn").on("click",function(){
+    $(".form-group").on("click",".new_card_btn",function(){
+        
         $(".row").append('<div class="col-lg-4 d-flex im_hi_div my-2 "><div class="card"><img src="' + imagePath + '" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' + title + '</h5><p class="card-text">' + card_body + '</p><button class="btn btn-outline-light btn-sm mx-2 btn_delete_card">Delete Card</button><button class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></button></div><div class="card-footer"><small class="text-muted">Last updated 3 mins ago</small></div></div></div>');
-        $(".new_card_btn").remove();
+        // $(this).parentsUntil("form").remove();
+       $(".new_card_btn").remove();
+       
+       
+        // $(".new_card_btn").remove();parentsUntil(".new__card__")
+        
+        // $(".new_card_btn_del").remove();
+        // $(".new__card__").remove()
+    //    $(".form-group").remove(); 
+    
     })
+        
+    // $(".form-group").on("click",".new_card_btn",function(){
+    //     $(this).parent().remove()});
+    
+
+ 
+
+    // $(".new_card_btn_del").on("click",function(event){
+    //     $(this).parentsUntil(".to_help_del").remove();
+    //     // $(".new_card_btn_del").remove();
+    //     // $(".new_card_btn").remove();
+    //     // event.stopPropagation();
+       
+    // })
 
 
    
@@ -103,7 +140,7 @@ $(".row").on("click",".btn_delete_card",function(){
     $(".row").on("click",".card-img-top",function(){
         $(".card-body").toggle();
             $(".im_hi_div").toggleClass("d-flex");
-    } )
+    } );
 
 
 // $(".btn_delete_card").on("click",'parentsUntil(".row ")', function(){
@@ -125,3 +162,4 @@ $(".row").on("click",".btn_delete_card",function(){
 //     $(this).parentsUntil(".row ").remove();
 // } )
 // C O R R  E C T
+// });
